f72ba25e217f9364f752a6acdf2a2d01c28e6ad6
maahin2005 <mahin.malek2005@gmail.com>
2024-07-21 12:47:55 +0530

    contact section update

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function page() {
-  return <div>Contact</div>;
+  return (
+    <div className="p-6 shadow-md m-auto my-10 w-[70%] ">
+      <h1 className="text-3xl font-bold mb-4 border-b-2 w-max">Contact Us!</h1>
+      <div>
+        <p>will Update soon!</p>
+      </div>
+    </div>
+  );
 }
 
 export default page;
