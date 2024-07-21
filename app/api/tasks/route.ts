bda58ae00272aa5581e2142d0d3ff379dd408d89
maahin2005 <mahin.malek2005@gmail.com>
2024-07-21 11:33:37 +0530

    Initializing backend with POST and GET requests.

---
+++ app/api/tasks/route.ts
@@ -0,0 +1,50 @@
+import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const POST = async (req: NextRequest) => {
+  try {
+    const { title, description, status } = await req.json();
+
+    // Validate the input
+    if (
+      typeof title !== "string" ||
+      typeof description !== "string" ||
+      typeof status !== "boolean"
+    ) {
+      return NextResponse.json({ error: "Invalid input" }, { status: 400 });
+    }
+
+    const newTodo = await prisma.todos.create({
+      data: {
+        title,
+        description,
+        status,
+        createdAt: new Date(),
+      },
+    });
+
+    return NextResponse.json({ data: newTodo }, { status: 201 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: (error as Error).message },
+      { status: 500 }
+    );
+  }
+};
+
+export const GET = async (req: NextRequest, res: NextResponse) => {
+  try {
+    const todos = await prisma.todos.findMany();
+
+    return NextResponse.json({ data: todos, status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: (error as Error).message },
+      { status: 500 }
+    );
+  }
+};
